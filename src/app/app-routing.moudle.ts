1b1ff66b492295afa6519c0abb993e0640dbf36a
EthanChen <Ethanchen@louiss-MacBook-Pro-3.local>
2017-12-13 19:04:44 -0600

    homework2
    homework2

---
+++ src/app/app-routing.moudle.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {RouterModule,Routes} from '@angular/router';
+import {ProductDetailComponent} from './product-detail-component'
+import { AppComponent } from './app.component';
+const appRoutes:Routes = [
+    {
+        path:'home',component:AppComponent,
+    },
+    {path:':detail',component:ProductDetailComponent},
+];
+@NgModule({
+    imports:[
+            appRoutes,
+            {
+                enableTracing: true, // <-- debugging purposes only
+            }
+        )
+    ],
+    exports:[RouterModule]
+})
+export class AppRoutingModule{}
